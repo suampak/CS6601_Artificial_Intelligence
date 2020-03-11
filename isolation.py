0f47d8dbc2765367163118a9e99570c2bd6b55a5
suampak <suampa.ket@gmail.com>
2020-03-11 20:45:49 +0000

    implement minimax algorithms

--- isolation.py
+++ isolation.py
@@ -1,24 +1,22 @@
 import random
+import copy
 
 class IsolationGame:
-    def __init__(self, m, n):
+    def __init__(self, m, n, isP1):
         '''
             INPUT
             m = row of board
             n = column of board
         '''
-        '''
-            0: not visited
-            1: visited
-            2: marked visited (for searching)
-        '''
-        self.isVisited = [[0 for j in range(n)] for i in range(m)]
+        self.isVisited = [[False for j in range(n)] for i in range(m)]
         '''
             p1 is actual player
             p2 is AI
         '''
         self.p1 = [-1, -1]
         self.p2 = [-1, -1]
+        self.isP1 = isP1
+        self.nextMoves = self.generateAllMoves()
 
     def __str__(self):
         '''
@@ -38,68 +36,116 @@ class IsolationGame:
             ret.append('\n')
         return ''.join(ret)
 
-    def selMove(self, i, j, isP1):
+    def selMove(self, i, j):
         '''
             check if the provided coordinate i, j is eligible for p1 (or p2)
             and change the board status accordingly
         '''
-        pos = self.p1 if isP1 else self.p2
-        if (pos == [-1,-1] and self.isValidMove(i,j)) or \
-           [i,j] in self.generateAllMoves(isP1):
-            self.isVisited[i][j] = 1
+        pos = self.p1 if self.isP1 else self.p2
+        if [i,j] in self.nextMoves:
+            self.isVisited[i][j] = True
             pos[0] = i
             pos[1] = j
+            self.isP1 = not self.isP1
+            self.nextMoves = self.generateAllMoves()
             return True
         return False
 
-    def calNextMoveImpl(self, isP1):
-        pos = self.p1 if isP1 else self.p2
-        i,j = random.choice(self.generateAllMoves(isP1))
-        self.isVisited[i][j] = 1
+    def calNextMove(self):
+        pos = self.p1 if self.isP1 else self.p2
+        prevPos = [pos[0],pos[1]]
+        for nexti, nextj in self.nextMoves:
+            self.isVisited[nexti][nextj] = True
+            pos[0] = nexti
+            pos[1] = nextj
+            if IsolationGame.calNextMoveImpl(self.isVisited, self.p1, self.p2, not self.isP1):
+                self.isP1 = not self.isP1
+                self.nextMoves = self.generateAllMoves()
+                return nexti, nextj
+            self.isVisited[nexti][nextj] = False
+            pos[0] = prevPos[0]
+            pos[1] = prevPos[1]
+
+        return self.calNextMoveRandom()
+
+    def calNextMoveRandom(self):
+        pos = self.p1 if self.isP1 else self.p2
+        i,j = random.choice(self.nextMoves)
+        self.isVisited[i][j] = True
         pos[0] = i
         pos[1] = j
+        self.isP1 = not self.isP1
+        self.nextMoves = self.generateAllMoves()
         return i,j
 
-    def generateAllMoves(self, isP1):
+    @staticmethod
+    def calNextMoveImpl(isVisited, p1, p2, isP1):
+        '''
+            Return True if p1 loses
+        '''
+        allMoves = IsolationGame.generateAllMovesImpl(isVisited, p1, p2, isP1)
+        if len(allMoves) == 0:
+            return True if isP1 else False
+
+        pos = p1 if isP1 else p2
+        prevPos = [pos[0],pos[1]]
+        for nexti, nextj in allMoves:
+            isVisited[nexti][nextj] = True
+            pos[0] = nexti
+            pos[1] = nextj
+            isP2Win = IsolationGame.calNextMoveImpl(isVisited, p1, p2, not isP1)
+            isVisited[nexti][nextj] = False
+            pos[0] = prevPos[0]
+            pos[1] = prevPos[1]
+            if (isP1 and not isP2Win) or (not isP1 and isP2Win):
+                return isP2Win
+
+        return isP1
+
+    def generateAllMoves(self):
+        return self.generateAllMovesImpl(self.isVisited, self.p1, self.p2, self.isP1)
+
+    @staticmethod
+    def generateAllMovesImpl(isVisited, p1, p2, isP1):
         '''
             generate all possible next moves for p1 (or p2)
         '''
-        curri, currj = self.p1 if isP1 else self.p2
+        curri, currj = p1 if isP1 else p2
         ret = []
         if curri == -1 and currj == -1:
             ret = []
-            for i in range(len(self.isVisited)):
-                for j in range(len(self.isVisited[0])):
-                    ret.append([i,j])
+            for i in range(len(isVisited)):
+                for j in range(len(isVisited[0])):
+                    if not isVisited[i][j]:
+                        ret.append([i,j])
         else:
             for diffi, diffj in [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]:
                 mult = 1
-                while self.isValidMove(curri+diffi*mult, currj+diffj*mult):
+                while IsolationGame.isValidMove(isVisited, curri+diffi*mult, currj+diffj*mult):
                     ret.append([curri+diffi*mult, currj+diffj*mult])
                     mult += 1
         return ret
 
-    def isValidMove(self, i, j):
-        return i >= 0 and i < len(self.isVisited) and \
-               j >= 0 and j < len(self.isVisited[0]) and \
-               self.isVisited[i][j] == 0
+    @staticmethod
+    def isValidMove(isVisited, i, j):
+        return i >= 0 and i < len(isVisited) and \
+               j >= 0 and j < len(isVisited[0]) and \
+               not isVisited[i][j]
 
 '''test'''
-game = IsolationGame(5,5)
-isP1 = False
-print 'Start 5x5 Isolation Game...'
-while len(game.generateAllMoves(isP1)) > 0:
-    if isP1:
+game = IsolationGame(4,4,True)
+print 'Start 4x4 Isolation Game...'
+while len(game.nextMoves):
+    if game.isP1:
         i, j = -1, -1
-        while not game.selMove(i,j,isP1):
+        while not game.selMove(i,j):
             i, j = input('Choose next move: ')
         print 'Player chooses {},{}'.format(i,j)
     else:
-        i, j = game.calNextMoveImpl(isP1)
+        i, j = game.calNextMove()
         print 'Opponent chooses {},{}'.format(i,j)
     print game
-    isP1 = not isP1
-if isP1:
+if game.isP1:
     print 'Opponent wins :('
 else:
     print 'Player wins :)'
